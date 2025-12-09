9cf9338529765c62c3be842ea89724b58c543995
astrid <uhhhAstrid@gmail.com>
2025-12-08 19:34:18 -0500

    added example

--- Hi/Untitled2.py
+++ Hi/Untitled2.py
@@ -1,1 +1,2 @@
-print("hi)")
+def ZOcode():
+    print("hi)")
--- astrid.py
+++ astrid.py
@@ -2,5 +2,6 @@ import devin as dev
 from Hi import Untitled2 as ZO 
 
 dev.sayHello()
-
 print("Hello from astrid! This is my contribution to the project.")
+
+ZO.ZOcode()
